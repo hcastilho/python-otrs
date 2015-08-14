a707f1e55ada15a19d98ca6e90e0cf2294ddc6bc
Hugo Castilho <hugo.p.castilho@telecom.pt>
2015-08-14 16:48:06 +0100

    Change SOAP_ENVELOPE to unicode

--- otrs/client.py
+++ otrs/client.py
@@ -42,7 +42,7 @@ def authenticated(func):
         return func(self,*args, **kwargs)
     return add_auth
 
-SOAP_ENVELOPPE = """
+SOAP_ENVELOPPE = u"""
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
                   xmlns="http://www.otrs.org/TicketConnector/">
   <soapenv:Header/>
